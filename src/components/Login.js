aa170353832ce208a69a0a4291be6f6a32cc03ed
awe peter <awe.petersq@gmail.com>
2023-06-01 09:43:34 +0100

    clientid updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,16 +2,38 @@ import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import {useNavigate} from 'react-router-dom';
+import jwt_decode from "jwt-decode";
 import {fcGoogle} from 'react-icons/fc';
 import favicon from '../assets/favicon.png';
 import logo from '../assets/logo.png';
 import logoWhite from '../assets/logowhite.png';
 import share from '../assets/share.mp4';
 
+import { client } from '../client';
+
 const Login = () => {
+    const navigate = useNavigate();
 
     const responseGoogle = (response) => {
-          console.log(response)
+         localStorage.setItem('user', JSON.stringify(response.profileObj));
+         var decodedHeader = jwt_decode(response.credential);
+         const { name, sub, imageUrl } = decodedHeader;
+
+         //schema database
+         const doc = {
+            _id: sub,
+            _type: 'user',
+            name: name,
+            image: imageUrl
+         }
+         
+
+         //check if value exist on database 
+         client.createIfNotExists(doc)
+         .then(() => {
+            navigate('/', {replace: true})
+
+         })
 
     }
   return (
@@ -39,12 +61,8 @@ const Login = () => {
                         <fcGoogle className = "mr-4"/> sign in with google 
                     </button>
                 )}
-                onSuccess={credentialResponse => {
-                    console.log(credentialResponse);
-                  }}
-                  onError={() => {
-                    console.log('Login Failed');
-                  }}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
                 cookiePolicy='single_host_origin'
                 />
             </div>
